9190cec05d1f15623122024efa803e3d801d18be
pavelgordeev <pvgord@icloud.com>
2017-07-06 11:43:37 +0400

    Now can count all roots that contains in ranges > 1, have to count roots in range that contains 1

--- src/ru/ssau/Main.java
+++ src/ru/ssau/Main.java
@@ -1,28 +1,79 @@
 package ru.ssau;
 
-import java.math.BigInteger;
-import java.util.HashMap;
+import io.github.miraclefoxx.math.BigDecimalMath;
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main{
 
-    public static void main( String[] args ){
-        BigInteger a = BigInteger.valueOf( 1 ), // without OO
-                b = 2, // with OO
+    public static void main( String[] args ) throws IOException{
+        BigDecimal a = 3 * Math.PI / 2;
+        Integer precision = 15;
+        BigDecimal                        two = 2 , pi = BigDecimal.valueOf( Math.PI );
+        List<Pair<BigDecimal, BigDecimal>> ranges = Stream.iterate( 1 , integer -> integer + 1 )
+//                    π*n/a - π/2a
+                .map( integer -> BigDecimal.valueOf( integer ).multiply( pi ).divide( a , BigDecimal.ROUND_HALF_EVEN )
+                        .subtract( pi.divide( two.multiply( a ) , BigDecimal.ROUND_HALF_EVEN ) ) )
+//                just where ( cos( a * x ) )' > 0
+                .filter( zeroPoint -> -Math.sin( a.multiply( zeroPoint ).doubleValue() ) > 0 )
+//                .filter( point -> point.compareTo( BigDecimal.valueOf( 1 ) ) == 1 )
+                .map( leftBorder -> new Pair<>( leftBorder , leftBorder.add( two.multiply( pi.divide( a.multiply( two ) , BigDecimal.ROUND_HALF_EVEN ) ) ) ) )
+                .limit( 50 )
+                .collect( Collectors.toList() );
+        for( Pair<BigDecimal, BigDecimal> range : ranges ){
+            System.out.printf( "[%s;%s] " , range.getKey().setScale( precision , BigDecimal.ROUND_HALF_UP ) ,
+                               range.getValue().setScale( precision , BigDecimal.ROUND_HALF_UP ) );
+            System.out.println( isRangeContainsDot( range.getKey() , range.getValue() , BigDecimal.ONE ) );
+        }
+//        System.setOut( new PrintStream( Files.newOutputStream( Paths.get( "/Users/pavelgordeev/Desktop/dots.txt" ) , StandardOpenOption.CREATE_NEW ) , true , "utf-8" ) );
+//        for( Pair<BigDecimal, BigDecimal> range : ranges ){
+//            String leftValue = calculate( range.getKey(), a, precision ).toString().replace( ".", "," ),
+//                    rightValue = calculate( range.getValue(), a, precision ).toString().replace( ".", "," );
+//            System.out.printf( "%s 0 0\n" , leftValue );
+//            System.out.printf( "%s 0 0\n" , rightValue );
+//        }
+    }
 
-                c1 = a.negate().add( b.multiply( b ) ).add( b.divide( a ) ), // without OO
-                c2 = -a + b * b + b / a; // with OO
+    private static Boolean isRangeContainsDot( BigDecimal leftBorder , BigDecimal rightBorder , BigDecimal dot ){
+        return leftBorder < dot && dot < rightBorder;
+    }
 
-        System.out.println( a + b );
-        System.out.println( c1 );
-        System.out.println( c2 );
+    private static BigDecimal calculate( BigDecimal xn, BigDecimal a, Integer precision ){
+        BigDecimal eps = BigDecimal.valueOf( Double.valueOf( "1e-" + precision.toString() ) );
+        return calculate( xn , a , eps );
+    }
 
-        if( c1.compareTo( c2 ) < 0 || c1.compareTo( c2 ) > 0 ) // without OO
-            System.out.println( "impossible" );
-        if( c1 < c2 || c1 > c2 ) // with OO
-            System.out.println( "impossible" );
+    private static BigDecimal calculate( BigDecimal xn, BigDecimal a, BigDecimal eps ){
+        BigDecimal                       one              = 1;
+        Function<BigDecimal, BigDecimal> originalFunction = x -> BigDecimalMath.cos( a.multiply( x ).setScale( 50 , BigDecimal.ROUND_HALF_EVEN ) ) - one.divide( x, 50, BigDecimal.ROUND_HALF_EVEN );
+        Function<BigDecimal, BigDecimal> derivative       = x -> -a * BigDecimalMath.sin( a.multiply( x ).setScale( 50 , BigDecimal.ROUND_HALF_EVEN ) ) + one.divide( x.pow( 2 ), 50 , BigDecimal.ROUND_HALF_EVEN );
+        BigDecimal                       nextStep         = originalFunction.apply( xn ).divide( derivative.apply( xn ), BigDecimal.ROUND_HALF_EVEN );
+        BigDecimal                       xn_1             = xn - nextStep;
+        if( nextStep.abs() < eps ) return xn_1;
+        return calculate( xn_1, a , eps );
+    }
 
-        HashMap<String, String> map = new HashMap<>();
-        if( !map.containsKey( "qwe" ) ) map.put( "qwe", map.get( "asd" ) ); // without OO
-        if( map[ "qwe" ] == null ) map[ "qwe" ] = map[ "asd" ]; // with OO
+    private static CountOfValues countValues( BigDecimal valueOfFunctionInBorders, BigDecimal valueOfFunctionInMiddle,
+                                              Integer countOfImportantNumbersAfterDot ){
+        BigDecimal eps = BigDecimal.valueOf( Double.valueOf( "1e-" + countOfImportantNumbersAfterDot.toString() ) );
+        if( valueOfFunctionInMiddle.abs() < eps  ) return CountOfValues.One;
+        else if( valueOfFunctionInBorders.abs() < eps ) return CountOfValues.Two;
+        else if( valueOfFunctionInBorders.signum() * valueOfFunctionInMiddle.signum() == 1 ) return CountOfValues.NoOne;
+        else return CountOfValues.Two;
+    }
+
+    enum CountOfValues{
+        NoOne, One, Two
     }
 }
+
---
+++ src/ru/ssau/NewtonCalculator.java
@@ -0,0 +1,8 @@
+package ru.ssau;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+class NewtonCalculator{
+
+}
